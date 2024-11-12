80c18f7b790b9b136dfcaeca8d58018c258253a1
ABHIN M <abhin18m@gmail.com>
2024-11-13 00:37:27 +0530

    header video mp4

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
                         <h1 className="title-sd">Shree Devi</h1>
                         <h1 className="title">SAMBHRAM&apos;24</h1>
                         <p className="description">
-                            Mog a shawty, bang a baddie, lets get sambhram !
+                            W"Harmonize with Visionaries" invites attendees to join an esteemed group.
                         </p>
                         <p className="date">On Dec 06 & 07</p>
                         <button className="btn-white">
