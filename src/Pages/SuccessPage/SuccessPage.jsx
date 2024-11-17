175037ec3d860ef1869a61f588f1703d23dc4fbb
ABHIN M <abhin18m@gmail.com>
2024-11-18 01:17:49 +0530

    success page verify

--- src/Pages/SuccessPage/SuccessPage.jsx
+++ src/Pages/SuccessPage/SuccessPage.jsx
@@ -59,6 +59,7 @@ const SuccessPage = () => {
                     <div className="success-up">
                         <img src="https://cdn2.iconfinder.com/data/icons/greenline/512/check-512.png" alt="Checkmark" />
                         <h2>Registered successfully</h2>
+                       <p>{orderId}</p>
                         <p>Ticket will be sent to your number shortly</p>
                     </div>
                     <div className="success-down">
