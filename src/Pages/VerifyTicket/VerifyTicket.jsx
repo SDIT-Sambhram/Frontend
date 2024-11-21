5be2839ec5d5494ace0991291dfa510f4a9c6461
ABHIN M <abhin18m@gmail.com>
2024-11-21 12:13:39 +0530

    verify page

--- src/Pages/VerifyTicket/VerifyTicket.jsx
+++ src/Pages/VerifyTicket/VerifyTicket.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useState, useContext, useEffect } from 'react';
 import { useLocation } from 'react-router-dom'; // Import useLocation
 import { StoreContext } from '../../Contexts/StoreContext';
+import Preloader from '../../Components/Preloader/Preloader'
 
 const VerifyTicket = () => {
     const { eventDatas } = useContext(StoreContext);
@@ -16,9 +17,9 @@ const VerifyTicket = () => {
     const id = searchParams.get('id'); // Get the `id` from query parameters
 
     let currentCoupon = items.length === 4 ? "DRAGON"
-    : items.length === 3 ? "FLAME"
-      : items.length === 2 ? "FANG"
-        : "EMBER";
+        : items.length === 3 ? "FLAME"
+            : items.length === 2 ? "FANG"
+                : "EMBER";
 
     useEffect(() => {
         const verifyTicket = async () => {
@@ -31,7 +32,7 @@ const VerifyTicket = () => {
             try {
                 const verifyResponse = await axios.post(
                     "https://o83h8nltlc.execute-api.ap-south-1.amazonaws.com/api/v1/auth/verify/ticket",
-                    { id } // Pass the fetched ID
+                    { id }
                 );
 
                 const participant = verifyResponse.data;
@@ -42,12 +43,10 @@ const VerifyTicket = () => {
                     return;
                 }
 
-                // Get registration IDs
                 const registeredEventIds = participant.registrations.map(reg => reg.event_id);
                 console.log('Registered Event IDs:', registeredEventIds);
 
-                // Filter events
-                const filteredEvents = eventDatas.filter(event => 
+                const filteredEvents = eventDatas.filter(event =>
                     registeredEventIds.includes(event._id)
                 );
                 console.log('Filtered Events:', filteredEvents);
@@ -73,64 +72,90 @@ const VerifyTicket = () => {
         if (eventDatas && eventDatas.length > 0) {
             verifyTicket();
         }
-    }, [id, eventDatas]); // Depend on `id` and `eventDatas`
+    }, [id, eventDatas]);
 
     return (
         <div className="verify-ticket">
             <div className="verification-container">
                 <div className="participant-details">
-                    {participantData?<img src="https://png.pngtree.com/png-clipart/20230805/original/pngtree-shabby-stamp-with-inscription-verified-trusted-seal-label-vector-picture-image_9855465.png" alt="" />:<p><img className='unverified-image' src="https://static.vecteezy.com/system/resources/previews/009/384/246/non_2x/tick-and-cross-clipart-design-illustration-free-png.png" alt="" /></p>}
-                
-                    <h3>Participant Details{!participantData?" Not Found":""}</h3>
-                    {participantData && (
+                    {participantData ? <img src="https://png.pngtree.com/png-clipart/20230805/original/pngtree-shabby-stamp-with-inscription-verified-trusted-seal-label-vector-picture-image_9855465.png" alt="" /> : <p><img className='unverified-image' src="https://static.vecteezy.com/system/resources/previews/009/384/246/non_2x/tick-and-cross-clipart-design-illustration-free-png.png" alt="" /></p>}
+
+                    <h3>{participantData ? "PARTICIPANT DETAILS" : ""}</h3>
+                    {participantData ?
                         <>
+                            <p>{participantData._id || 'N/A'}</p>
                             <div className="participant-detail">
-                                <h6>Id</h6>
-                                <p>{participantData._id || 'N/A'}</p>
-                            </div>
-                            <div className="participant-detail">
-                                <h6>Pass Type</h6>
-                                <p>{currentCoupon || 'N/A'}</p>
-                            </div>
-                            <div className="participant-detail">
-                                <h6>Name</h6>
+                                <p>Name</p>
                                 <p>{participantData.name || 'N/A'}</p>
                             </div>
                             <div className="participant-detail">
-                                <h6>College</h6>
-                                <p>{participantData.college || 'N/A'}</p>
-                            </div>
-                            <div className="participant-detail">
-                                <h6>Phone</h6>
+                                <p>Phone</p>
                                 <p>{participantData.phone || 'N/A'}</p>
                             </div>
                             <div className="participant-detail">
-                                <h6>USN</h6>
+                                <p>USN</p>
                                 <p>{participantData.usn || 'N/A'}</p>
                             </div>
-                        </>
-                    )}
+                            <div className="participant-detail">
+                                <p>College</p>
+                                <div className="college-name">
+                                <p>{participantData.college || 'N/A'}</p>
+                                </div>
+                            </div>
+                            
+                           
+                        </> : <div className="spinner"></div>
+                    }
                 </div>
-                  
-                <div className="participated-events">
-                    <h4>EVENTS REGISTERED</h4>
+                {participantData ? <div className="participated-events">
+                    <h4>REGISTERED EVENTS</h4>
                     <div className="events-list">
                         {items && items.length > 0 ? (
-                            items.map(event => (
-                                <div className="participated-event" key={event._id}>
-                                    <ul>
-                                        <li><p>{event.eventName}</p></li>
-                                    </ul>
-                                    <p>
-                                        {event.date}
-                                    </p>
-                                </div>
+                            items.map((event, index) => (
+                                <>
+                                    <div className="participant-events-list">
+                                        <b>{event.eventName}</b>
+                                        <div className="participated-event" key={event._id}>
+                                            <p>Date</p>
+                                            <p>
+                                                {event.date}
+                                            </p>
+                                        </div>
+                                        <div className="participated-event">
+                                            <p>Time</p>
+
+                                            <p>{event.time}</p>
+                                        </div>
+                                        <div className="participated-event">
+                                            <p>Venue</p>
+
+                                            <p>{event.venue}</p>
+                                        </div>
+                                        <div className="participated-event">
+                                            <p>Student Coordinater</p>
+
+                                            <p>{event.studentCoordinator}</p>
+                                        </div>
+                                        <div className="participated-event">
+                                            <p>Contact</p>
+
+                                            <p>{event.studentCoordinatorContact}</p>
+                                        </div>
+
+
+                                    </div>
+
+                                </>
+
                             ))
                         ) : (
                             <p>No events found</p>
                         )}
                     </div>
-                </div>
+                </div> :
+                    <></>}
+
+
             </div>
         </div>
     );
